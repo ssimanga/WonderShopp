0931bfc6213dc49fa59ea05bea8f195286f7be0b
Simanga Shabalala <simangae@gmail.com>
2023-08-28 10:01:33 +0200

    Models, Reposities and Dependancy Injection

---
+++ WonderShopp.Core/Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WonderShopp.Core.Models;
+
+namespace WonderShopp.Core.Contracts
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> Collection();
+        void Commit();
+        void Delete(string Id);
+        T Find(string Id);
+        void Insert(T t);
+        void Update(T t);   
+
+    }
+}
