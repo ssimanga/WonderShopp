26df05f6d1b8f5fec51750bfe17a08d3d97bcf04
Simanga Shabalala <simangae@gmail.com>
2023-09-13 08:34:43 +0200

    Cart Service

--- WonderShopp.Core/ViewModels/CartItemVM.cs
+++ WonderShopp.Core/ViewModels/CartItemVM.cs
@@ -8,5 +8,10 @@ namespace WonderShopp.Core.ViewModels
 {
     public class CartItemVM
     {
+        public string Id { get; set; }
+        public int Quantity { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
     }
 }
